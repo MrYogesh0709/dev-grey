014f4af773493825675c30af1743f2b8283618aa
Mr.Yogesh <yogeshvanzara98@gmail.com>
2023-04-12 14:19:21 +0530

    back to code

--- controllers/authController.js
+++ controllers/authController.js
@@ -29,8 +29,7 @@ const handleLogin = async (req, res) => {
     const refreshToken = jwt.sign(
       { username: foundUser.username },
       process.env.REFRESH_TOKEN_SECRET,
-      // { expiresIn: "1d" }
-      { expiresIn: "15s" }
+      { expiresIn: "1d" }
     );
     // Saving refreshToken with current user
     foundUser.refreshToken = refreshToken;
